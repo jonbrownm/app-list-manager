22484167cfd2e35aeda03f2e04aa0bdfd921114d
Jonathan Brown <jonathan.brown@bt.com>
2018-04-23 16:53:54 +0100

    Added non-function delete button

--- src/js/components/listManager/addItemToList.js
+++ src/js/components/listManager/addItemToList.js
@@ -4,10 +4,12 @@ export const addItemToList = (elementTable, elementUserInput) => {
 	if (elementUserInput) {
 
 		var newRow = elementTable.insertRow(0),
-			newCell = newRow.insertCell(0);
+			newCellWithInput = newRow.insertCell(0),
+			newCellWithDelete = newRow.insertCell(0);
 
 
-		newCell.innerHTML = elementUserInput;
+		newCellWithInput.innerHTML = "<button class='button small secondary app-list-manager_input-delete'>Delete</button>";
+		newCellWithDelete.innerHTML = elementUserInput;
 
 	}
 
