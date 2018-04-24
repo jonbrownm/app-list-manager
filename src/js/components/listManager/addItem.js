f0442773df0dde1ec854ecfbf9f362d959158fa4
Jonathan Brown <jonathan.brown@bt.com>
2018-04-24 11:27:30 +0100

    Added to local storage

--- src/js/components/listManager/addItem.js
+++ src/js/components/listManager/addItem.js
@@ -7,12 +7,15 @@ export const addItem = (elementTable, elementUserInput) => {
 
 		var newRow = elementTable.insertRow(0),
 			newCellWithInput = newRow.insertCell(0),
-			newCellWithDelete = newRow.insertCell(0);
+			newCellWithDelete = newRow.insertCell(0),
+			newItemId = returnItemId();
 
 
-		newRow.setAttribute("data-item-id", returnItemId(), 0);
+		newRow.setAttribute("data-item-id", newItemId, 0);
 		newCellWithInput.innerHTML = "<button class='button small secondary app-list-manager_input-delete' data-item-delete>Delete</button>";
 		newCellWithDelete.innerHTML = elementUserInput;
+
+		localStorage.setItem(newItemId, elementUserInput);
 
 	}
 
--- src/js/components/listManager/deleteItem.js
+++ src/js/components/listManager/deleteItem.js
@@ -2,15 +2,16 @@
 export const deleteItem = (elementUserDelete) => {
 
 	var currentTableRows = document.querySelectorAll("[data-app-list-manager] table tbody tr"),
-		currentActiveId = elementUserDelete.parentElement.parentElement.getAttribute("data-item-id"),
+		currentId = elementUserDelete.parentElement.parentElement.getAttribute("data-item-id"),
 		currentTableRowId;
 
 
 	[].forEach.call(currentTableRows, function(row){
 		currentTableRowId = row.getAttribute("data-item-id");
 
-		if (currentActiveId === currentTableRowId) {
+		if (currentId === currentTableRowId) {
 			row.remove();
+			localStorage.removeItem(currentId);
 		}
 		
 	});
